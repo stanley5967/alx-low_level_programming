420da619119873273b9ebb01f82c3032718364d4
Obinugwu Stanley <chukwumastanley76@gmail.com>
2022-09-26 04:57:35 -0700

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Author: Eyo Saviour
- * Description: header file container prototypes used in task
- */
-
-#include <stddef.h>
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -18,6 +7,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to)g
-
-#endif
+void set_string(char **s, char *to);
