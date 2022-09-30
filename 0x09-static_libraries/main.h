864ce293e6a224556dfea4ac5220a76d254ba84e
Obinugwu Stanley <chukwumastanley76@gmail.com>
2022-09-30 04:17:20 -0700

    Create the static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
-int _isalpha(int c);
+int _isalpha(
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
@@ -18,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif /* !MAIN_H */
